db2843532e2108a9fd9e92542fc3e90b640e1c59
Adrish Paul <adrishpaul06902@gmail.com>
2026-02-28 22:28:02 +0530

    beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,14 +7,14 @@ android {
 
     namespace = "com.example.mypage"
 
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.mypage"
         minSdk = 31
-        targetSdk = 34
-        versionCode = 1
-        versionName = "Alpha_1"
+        targetSdk = 36
+        versionCode = 2
+        versionName = "Beta"
     }
 
     buildTypes {
@@ -25,15 +25,6 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-        }
-    }
-
-    splits {
-        abi {
-            isEnable = true
-            reset()
-            include("arm64-v8a", "armeabi-v7a")
-            isUniversalApk = false
         }
     }
 
@@ -62,6 +53,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.foundation)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/mypage/HomeScreen.kt
+++ app/src/main/java/com/example/mypage/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.example.mypage
 
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.*
@@ -10,6 +11,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.text.style.TextAlign
+import java.time.DayOfWeek
+import java.time.LocalDate
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -17,56 +24,60 @@ fun HomeScreen() {
     val routineData = mapOf(
 
         "Mon" to listOf(
-            RoutineItem("Operating Systems", "08:00", "09:00", "C25-B-117"),
-            RoutineItem("Computer Organization (COA)", "09:00", "10:00", "C25-B-117"),
-            RoutineItem("OOPJ", "10:00", "11:00", "—"),
-            RoutineItem("DBMS", "11:00", "12:00", "C25-B-115")
+            RoutineItem("OS", "08:00", "09:00", "C25-B-117"),
+            RoutineItem("COA", "09:00", "10:00", "C25-B-117"),
+            RoutineItem("OOPJ", "10:00", "11:00", "C25-B-117"),
+            RoutineItem("DBMS", "11:00", "12:00", "C25-B-115"),
+            RoutineItem("IEC", "12:00", "01:00", "C25-B-115"),
+            RoutineItem("DM", "01:00", "02:00", "C25-B-115")
         ),
 
         "Tue" to listOf(
-            RoutineItem("Discrete Mathematics (DM)", "10:00", "11:00", "C25-B-213"),
-            RoutineItem("Operating Systems", "11:00", "12:00", "C25-B-206")
+            RoutineItem("DM", "10:00", "11:00", "C25-B-213"),
+            RoutineItem("OS", "11:00", "12:00", "C25-B-206"),
+            RoutineItem("DBMS(L)", "12:00", "02:00", "C25-B-212(l)"),
+            RoutineItem("DBMS", "04:00", "05:00", "C25-A-301"),
+            RoutineItem("IEC", "05:00", "06:00", "C25-A-301")
         ),
 
         "Wed" to listOf(
-            RoutineItem("Operating Systems", "10:00", "11:00", "C25-B-217"),
-            RoutineItem("Discrete Mathematics (DM)", "11:00", "12:00", "—"),
-            RoutineItem("OOPJ", "12:00", "01:00", "C25-B-217")
+            RoutineItem("OS", "10:00", "11:00", "C25-B-217"),
+            RoutineItem("DM", "11:00", "12:00", "C25-B-217"),
+            RoutineItem("OOPJ", "12:00", "01:00", "C25-B-217"),
+            RoutineItem("VT", "03:00", "05:00", "-"),
         ),
 
         "Thu" to listOf(
-            RoutineItem("OS Lab", "10:00", "12:00", "C25-B-018(L)"),
-            RoutineItem("Discrete Mathematics (DM)", "12:00", "01:00", "C25-B-120")
+            RoutineItem("OS(L)", "9:00", "11:00", "C25-B-018(L)"),
+            RoutineItem("DM", "11:00", "12:00", "C25-B-120"),
+            RoutineItem("IEC", "12:00", "01:00", "C25-B-120"),
+            RoutineItem("COA", "01:00", "02:00", "C25-B-120")
         ),
 
         "Fri" to listOf(
             RoutineItem("DBMS", "08:00", "09:00", "C25-A-206"),
-            RoutineItem("OOPJ Lab", "09:00", "11:00", "C25-B-102(L)"),
-            RoutineItem("OOPJ", "11:00", "12:00", "C25-B-113")
+            RoutineItem("OOPJ(L)", "09:00", "11:00", "C25-B-102(L)"),
+            RoutineItem("OOPJ", "11:00", "12:00", "C25-B-113"),
+            RoutineItem("DM", "12:00", "01:00", "C25-B-113")
         )
     )
 
-    val days = listOf("Mon", "Tue", "Wed", "Thu", "Fri", "Sat")
-    var selectedDay by remember { mutableStateOf(0) }
-
+    val days = listOf("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat")
+    val today = LocalDate.now().dayOfWeek
+    val initialIndex = when (today) {
+        DayOfWeek.SUNDAY -> 0
+        DayOfWeek.MONDAY -> 1
+        DayOfWeek.TUESDAY -> 2
+        DayOfWeek.WEDNESDAY -> 3
+        DayOfWeek.THURSDAY -> 4
+        DayOfWeek.FRIDAY -> 5
+        DayOfWeek.SATURDAY -> 6
+    }
+    var selectedDay by remember { mutableStateOf(initialIndex) }
     val selectedDayName = days[selectedDay]
     val todayClasses = routineData[selectedDayName] ?: emptyList()
 
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = { Text("MyPage") },
-                actions = {
-                    IconButton(onClick = { }) {
-                        Icon(
-                            imageVector = Icons.Default.Person,
-                            contentDescription = "User"
-                        )
-                    }
-                }
-            )
-        }
-    ) { padding ->
+    Scaffold{ padding ->
 
         Column(
             modifier = Modifier
@@ -74,25 +85,89 @@ fun HomeScreen() {
                 .padding(padding)
 
         ) {
+            Surface(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                color = MaterialTheme.colorScheme.surface
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp, vertical = 20.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
 
-            ScrollableTabRow(selectedTabIndex = selectedDay) {
-                days.forEachIndexed { index, day ->
-                    Tab(
-                        selected = selectedDay == index,
-                        onClick = { selectedDay = index },
-                        text = { Text(day) }
+                    Text(
+                        text = "MyPage",
+                        style = MaterialTheme.typography.titleLarge,
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        color = MaterialTheme.colorScheme.secondary
                     )
+
+//                    IconButton(onClick = { }) {
+//                        Icon(
+//                            imageVector = Icons.Default.Person,
+//                            contentDescription = "User"
+//                        )
+//                    }
+                }
+            }
+
+            HorizontalDivider(
+                thickness = 1.5.dp,
+                color = MaterialTheme.colorScheme.outlineVariant
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp),
+                shape = RoundedCornerShape(20.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.surfaceContainerHigh
+                ),
+                elevation = CardDefaults.cardElevation(4.dp)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 12.dp, vertical = 16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(4.dp)
+                ) {
+                    days.forEachIndexed { index, day ->
+                        Box(
+                            modifier = Modifier
+                                .weight(1f)
+                                .aspectRatio(1f)
+                        ) {
+                            DayCircle(
+                                text = day,
+                                isSelected = selectedDay == index,
+                                onClick = { selectedDay = index }
+                            )
+                        }
+                    }
                 }
             }
 
             Spacer(modifier = Modifier.height(16.dp))
 
             if (todayClasses.isEmpty()) {
-                Text(
-                    text = "No Classes",
-                    modifier = Modifier.padding(16.dp),
-                    style = MaterialTheme.typography.bodyLarge
-                )
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "No Classes Today",
+                        style = MaterialTheme.typography.headlineSmall,
+                        color = MaterialTheme.colorScheme.secondary
+                    )
+                }
             } else {
                 LazyColumn(
                     modifier = Modifier.fillMaxSize(),
@@ -112,29 +187,31 @@ fun HomeScreen() {
 fun RoutineCard(item: RoutineItem) {
 
     Card(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(20.dp),
         colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.surface
+            containerColor = MaterialTheme.colorScheme.surfaceContainerHigh
         ),
         elevation = CardDefaults.cardElevation(
-            defaultElevation = 2.dp
-        ),
-        shape = MaterialTheme.shapes.large
+            defaultElevation = 6.dp
+        )
     ) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 16.dp, vertical = 14.dp),
+                .padding(horizontal = 18.dp, vertical = 18.dp),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
 
             Column {
                 Text(
                     text = item.subject,
-                    style = MaterialTheme.typography.titleMedium
+                    style = MaterialTheme.typography.titleMedium,
+                    color = MaterialTheme.colorScheme.primary.copy(alpha = 0.65f)
                 )
 
-                Spacer(modifier = Modifier.height(6.dp))
+                Spacer(modifier = Modifier.height(8.dp))
 
                 Text(
                     text = item.room,
@@ -156,6 +233,45 @@ fun RoutineCard(item: RoutineItem) {
                     color = MaterialTheme.colorScheme.onSurfaceVariant
                 )
             }
+        }
+    }
+}
+@Composable
+fun DayCircle(
+    text: String,
+    isSelected: Boolean,
+    onClick: () -> Unit
+) {
+
+    val scale by animateFloatAsState(
+        targetValue = if (isSelected) 1f else 0.95f,
+        label = "scaleAnim"
+    )
+
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .graphicsLayer {
+                scaleX = scale
+                scaleY = scale
+            },
+        shape = CircleShape,
+        color = if (isSelected)
+            MaterialTheme.colorScheme.primary
+        else
+            MaterialTheme.colorScheme.surface,
+        tonalElevation = if (isSelected) 4.dp else 1.dp,
+        onClick = onClick
+    ) {
+        Box(contentAlignment = Alignment.Center) {
+            Text(
+                text = text.take(1), // M T W T F
+                style = MaterialTheme.typography.labelLarge,
+                color = if (isSelected)
+                    MaterialTheme.colorScheme.onPrimary
+                else
+                    MaterialTheme.colorScheme.onSurface
+            )
         }
     }
 }
